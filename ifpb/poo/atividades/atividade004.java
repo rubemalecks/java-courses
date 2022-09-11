546edbcf6ff04ff309b8becd0e8fdd2b95f0ebe1
Rubem Álecks <rubemalecks@gmail.com>
2022-09-11 15:40:28 -0300

    atividade004
    04) Escreva um programa em Java que leia o valor da base e da altura de um
    triângulo e em seguida imprima na tela o valor de sua área. Lembrando que a
    fórmula da área é: A = (B x H)/2
    
    Exemplo:
    Base = 6 SAÍDA: Área => 9
    Altura = 3

---
+++ ifpb/poo/atividades/atividade004.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+import javax.sound.midi.Soundbank;
+
+public class atividade004 {
+    public static void main(String[] args) {
+        double base, altura, area;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Base = ");
+        base = sc.nextDouble();
+        System.out.print("Altura = ");
+        altura = sc.nextDouble(); 
+        System.out.println("Area => "+ (base*altura)/2); 
+    }
+    
+}
