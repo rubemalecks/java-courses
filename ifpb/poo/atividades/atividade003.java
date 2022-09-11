604117b771d2fc9aa0d3e6fd51e33032e8d3ee28
Rubem Álecks <rubemalecks@gmail.com>
2022-09-11 15:34:13 -0300

    atividade003
    03) Escreva um programa em Java que leia dois números da entrada A e B.
    Depois o programa deve exibir na tela o valor de AxB, Raiz de A e Raiz de B:
    Exemplo:
    A = 9 SAÍDA: AxB => 36
    B = 4 Raiz de A => 3
    Raiz de B => 2

---
+++ ifpb/poo/atividades/atividade003.java
@@ -0,0 +1,29 @@
+import java.net.SocketPermission;
+import java.util.Scanner;
+
+public class atividade003 {
+    public static void main(String[] args) {
+        int a, b;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("A: ");
+        a = sc.nextInt();
+
+        System.out.print("B: ");
+        b = sc.nextInt();
+
+        System.out.println("AxB => " + a * b);
+
+        double result_raiz_a;
+        result_raiz_a = Math.sqrt(a);
+        
+        System.out.println("Raiz de A => " + result_raiz_a);
+
+        double result_raiz_b;
+        result_raiz_b = Math.sqrt(b);
+        
+        System.out.println("Raiz de B => " + result_raiz_b);
+    }
+
+}
