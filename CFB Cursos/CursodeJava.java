a53e0cf96929959ac44cb6ef5670230d9fa4b8e9
Rubem Álecks <rubemalecks@gmail.com>
2022-09-11 13:28:22 -0300

    java-courses

---
+++ CFB Cursos/CursodeJava.java
@@ -0,0 +1,8 @@
+public class CursodeJava {
+    public static void main(String[] args) {
+        System.out.print("CFB Cursos");
+        System.out.println("Curso de Java\n");
+        System.out.printf("Canal:%s\nAno:%d", "CFB", 2021);
+
+    }
+}
---
+++ Variaveis/src/Variaveis.java
@@ -0,0 +1,8 @@
+
+public class Variaveis {
+    public static void main(String[] args) {
+        String nome = "Rubem alecks";
+        System.out.println("Nome: " + nome);
+    }
+
+}
