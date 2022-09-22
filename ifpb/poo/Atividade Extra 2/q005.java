71a3009659c83e8b5c2da0ae60f60204a11e01e1
Rubem Álecks <rubemalecks@gmail.com>
2022-09-22 20:30:50 -0300

    q005

--- ifpb/poo/Atividade Extra 2/q005.java
+++ ifpb/poo/Atividade Extra 2/q005.java
@@ -10,17 +10,26 @@ public class q005 {
         int soma = 0;
         double somad = 0;
         double media = 0;
-        int maior = 0;
         int menor = 0;
+        int maior = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Digite o " + i + "o valor: ");
             int vlr = sc.nextInt();
-
+            
             soma += vlr;
-            if (i == 0) {
+
+
+            if (i == 1) {
                 menor = vlr;
                 maior = vlr;
+            } else {
+                if (vlr < menor){
+                    menor = vlr;
+                }
+                if (vlr > maior){
+                    maior = vlr;
+                }
             }
             if (vlr % 2 == 0) {
                 cont_par += 1;
@@ -35,6 +44,8 @@ public class q005 {
         System.out.println(cont_impar + " impares.");
         System.out.println("A media: " + media);
         System.out.println("A soma: " + soma);
+        System.out.println("O menor: " + menor);
+        System.out.println("O maior: " + maior);
 
     }
 }
