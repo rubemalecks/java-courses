e99fb41672a2a1afa9ff90a893728ea8ce783bbd
Rubem Álecks <rubemalecks@gmail.com>
2022-10-05 18:59:48 -0300

    org

--- CFB Cursos/CursodeJava.java
+++
@@ -1,8 +0,0 @@
-public class CursodeJava {
-    public static void main(String[] args) {
-        System.out.print("CFB Cursos");
-        System.out.println("Curso de Java\n");
-        System.out.printf("Canal:%s\nAno:%d", "CFB", 2021);
-
-    }
-}
