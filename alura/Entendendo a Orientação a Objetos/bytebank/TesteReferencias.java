15cc2dfb55bc27b26ee0f5e51096beed882d652b
Rubem Álecks <rubemalecks@gmail.com>
2022-10-13 19:32:36 -0300

    comment

--- alura/Entendendo a Orientação a Objetos/bytebank/TesteReferencias.java
+++ alura/Entendendo a Orientação a Objetos/bytebank/TesteReferencias.java
@@ -5,11 +5,11 @@ public class TesteReferencias {
 
         System.out.println("saldo da primeira: " + primeiraConta.saldo);
 
-        Conta segundaConta = primeiraConta; // as 2x vão referenciar para uma mesma "Conta"
+        Conta segundaConta = primeiraConta; // as 2x vão referenciar para uma MESMA "Conta"
 
         segundaConta.saldo += 100;
         System.out.println("Segunda Conta: " + segundaConta.saldo);
         System.out.println("Saldo Primeira Conta: " + primeiraConta.saldo);
-    //Sendo assim se houver uma mudança ela irá afetar ambas as referencias.
+        // Sendo assim se houver uma mudança ela irá afetar ambas as referencias.
     }
 }
