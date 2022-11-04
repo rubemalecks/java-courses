63e13c5b87d5d74a93e245e4f92439f20f3e5c78
Rubem Álecks <rubemalecks@gmail.com>
2022-11-03 21:26:36 -0300

    ...

---
+++ ifpb/poo/2 - Classe e Objetos/Atributos, Metodos e construtores/atividade1/aluno_media/src/App.java
@@ -0,0 +1,9 @@
+import classes.aluno;
+
+public class App {
+    public static void main(String[] args){
+        aluno alunonovo = new aluno();
+        alunonovo.setNome("rubão");
+        System.out.println(alunonovo.getNome());
+    }
+}
