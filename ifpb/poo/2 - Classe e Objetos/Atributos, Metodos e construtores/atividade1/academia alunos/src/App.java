35e1afe77723706c627395b00efbe461bcbd5876
Rubem Álecks <rubemalecks@gmail.com>
2022-11-06 00:11:57 -0300

    academia

--- ifpb/poo/2 - Classe e Objetos/Atributos, Metodos e construtores/atividade1/academia alunos/src/App.java
+++ ifpb/poo/2 - Classe e Objetos/Atributos, Metodos e construtores/atividade1/academia alunos/src/App.java
@@ -1,9 +1,28 @@
 import alunos.registro;
+import java.util.Scanner;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
         registro aluno = new registro();
-        aluno.setNome("Rubem");
-        System.out.println(aluno.getNome());
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nome: ");
+        aluno.setNome(sc.nextLine());
+        System.out.print("Idade: ");
+        aluno.setIdade(sc.nextInt());
+        System.out.print("Peso: ");
+        aluno.setPeso(sc.nextDouble());
+        System.out.print("Altura: ");
+        aluno.setAltura(sc.nextDouble());
+        System.out.println("=============================");
+        System.out.println("\nRegistro do aluno: "+ aluno.getNome());
+        System.out.println("Idade: "+ aluno.getIdade());
+        System.out.println("Peso: "+ aluno.getPeso());
+        System.out.println("Altura: "+ aluno.getAltura());
+        System.out.printf("IMC: %.2f", aluno.imc());
+        System.out.print("\n"+aluno.temDesc(aluno.getIdade()));
+        sc.close();
+
     }
 }
--- ifpb/poo/2 - Classe e Objetos/Atributos, Metodos e construtores/atividade1/academia alunos/src/alunos/registro.java
+++ ifpb/poo/2 - Classe e Objetos/Atributos, Metodos e construtores/atividade1/academia alunos/src/alunos/registro.java
@@ -7,6 +7,19 @@ public class registro {
     private double peso;
     private double altura;
 
+    // Construtor
+    public registro() {
+
+    }
+
+    public registro(String nome, int idade, double peso, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.peso = peso;
+        this.altura = altura;
+
+    }
+
     // Métodos Getters e Setters
     public String getNome() {
         return nome;
@@ -14,6 +27,41 @@ public class registro {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    // Métodos da classe
+    public double imc() {
+        return (peso / altura) * 2;
+    }
+
+    public String temDesc(int idade){
+        if (idade < 18 ){
+            return "Menor de Idade, tem direito a desconto!";
+        } return "Maior de idade. Não tem desconto.";
     }
 
 }
