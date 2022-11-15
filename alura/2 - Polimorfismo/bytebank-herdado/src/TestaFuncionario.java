0c533b69a215bd52db0a78026ca11f81b084755e
Rubem Álecks <rubemalecks@gmail.com>
2022-11-15 18:26:54 -0300

    ...

---
+++ alura/2 - Polimorfismo/bytebank-herdado/src/TestaFuncionario.java
@@ -0,0 +1,16 @@
+public class TestaFuncionario {
+
+    public static void main(String[] args) {
+        
+        Funcionario nico = new Funcionario();
+        nico.setNome("Nico Steppat");
+        nico.setCpf("223355646-9");
+        nico.setSalario(2600);
+
+        System.out.println(nico.getNome());
+        System.out.println(nico.getBonificacao());
+
+    }
+
+    
+}
