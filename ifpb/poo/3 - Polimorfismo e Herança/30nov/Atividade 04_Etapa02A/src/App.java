aa80d68b312b60f7c9b6f3d89e68f288594afdb0
Rubem Álecks <46830421+rubemalecks@users.noreply.github.com>
2022-12-01 12:24:06 -0300

    Update App.java

--- ifpb/poo/3 - Polimorfismo e Herança/30nov/Atividade 04_Etapa02A/src/App.java
+++ ifpb/poo/3 - Polimorfismo e Herança/30nov/Atividade 04_Etapa02A/src/App.java
@@ -3,8 +3,8 @@ import classes.*;
 public class App {
     public static void main(String[] args) throws Exception {
         Poligono pol = new Poligono(10, 10);
-        Triangulo tri = new Triangulo(13, 7);
-        Quadrado quad = new Quadrado(7, 7);
+        Poligono tri = new Triangulo(13, 7);
+        Poligono quad = new Quadrado(7, 7);
 
         System.out.println("Area do Poligono: " + pol.CalculaArea());
         System.out.println("Area do Triangulo: " + tri.CalculaArea());
